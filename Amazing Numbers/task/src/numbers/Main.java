1fc73d8164477d30baa59ddd965546a9a730ad2e
Jovertki <wa9head@gmail.com>
2022-04-13 00:18:07 +0300

    finish

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -111,6 +111,58 @@ class AmazingNumber {
         return this.spy;
     }
 
+    public boolean isJumping() {
+        if (this.jumping != null)
+            return this.jumping;
+        if (value < 10) {
+            this.jumping = true;
+            return true;
+        }
+        String[] temp = value.toString().split("");
+        ArrayList<Long> nums = new ArrayList<>();
+        for (String s : temp) {
+            nums.add(Long.parseLong(s));
+        }
+        for (int i = 0; i <= nums.size() - 2; i++ ) {
+            if (nums.get(i) != nums.get(i + 1) + 1 &&
+                    nums.get(i) != nums.get(i + 1) - 1) {
+                this.jumping = false;
+                return false;
+            }
+        }
+        this.jumping = true;
+        return true;
+    }
+
+    public boolean isHappy() {
+        if (this.happy != null)
+            return this.happy;
+        if (value == 1) {
+            this.happy = true;
+            this.sad = false;
+            return true;
+        }
+        ArrayList<Long> repeats = new ArrayList<>();
+        long temp = value;
+        do {
+            repeats.add(temp);
+            String[] strs = Long.toString(temp).split("");
+            temp = 0;
+            for (String n : strs) {
+                temp += Long.parseLong(n) * Long.parseLong(n);
+            }
+        } while (temp != 1 && !repeats.contains(temp));
+        this.happy = temp == 1;
+        this.sad = !this.happy;
+        return this.happy;
+    }
+
+    public boolean isSad() {
+        if (this.sad != null)
+            return this.sad;
+        isHappy();
+        return this.sad;
+    }
 
 
     private final Long value;
@@ -123,10 +175,17 @@ class AmazingNumber {
     private Boolean spy = null;
     private Boolean square = null;
     private Boolean sunny = null;
+    private Boolean jumping = null;
+    private Boolean happy = null;
+    private Boolean sad = null;
 }
 
 public class Main {
     final static Scanner scanner = new Scanner(System.in);
+    final static String[] availableProps = {"EVEN", "ODD", "BUZZ", "DUCK",
+            "PALINDROMIC", "GAPFUL", "SPY", "SQUARE", "SUNNY", "JUMPING",
+            "HAPPY", "SAD", "-EVEN", "-ODD", "-BUZZ", "-DUCK", "-PALINDROMIC",
+            "-GAPFUL", "-SPY", "-SQUARE", "-SUNNY", "-JUMPING", "-HAPPY", "-SAD"};
 
     public static void main(String[] args) {
         printIntroduction();
@@ -143,8 +202,7 @@ public class Main {
             AmazingNumber num = new AmazingNumber(Long.parseLong(strs[0]));
             Long count = (strs.length == 1) ? null : Long.parseLong(strs[1]);
 
-            String[] props = (strs.length >= 3) ?
-                    Arrays.copyOfRange(strs, 2, strs.length) : null;
+            String[] props = parseProps(strs);
             if (errorCheck(num, count, props, strs.length)) {
                 if (num.getValue() == 0){
                     System.out.println("Goodbye!");
@@ -153,14 +211,45 @@ public class Main {
                     printOneNumProps(num);
                 } else if (strs.length == 2) {
                     printMultipleNumProps(num, count);
-                }  else if (strs.length == 3) {
-                    printNumsWithProp(num, count, props[0]);
-                } else if (strs.length == 4) {
-                    printNumsWithDoubleProps(num, count, props);
+                } else {
+                    printNumsWithProps(num, count, props);
                 }
             }
 
         } while (true);
+    }
+
+    static boolean hasAllProps(AmazingNumber num, String[] props) {
+        for (String prop : props) {
+            if (!hasProp(num, prop)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static void printNumsWithProps(AmazingNumber num, long count, String[] props) {
+        long fit = 0;
+        long n = num.getValue();
+        while (fit != count) {
+            AmazingNumber temp = new AmazingNumber(n);
+            if (hasAllProps(temp, props)) {
+                printPropsLine(temp);
+                fit++;
+            }
+            n++;
+        }
+    }
+
+    static String[] parseProps(String[] strs){
+        String[] props = (strs.length >= 3) ?
+                Arrays.copyOfRange(strs, 2, strs.length) : null;
+        if (props != null) {
+            for (int i = 0; i < props.length; i++)
+                props[i] = props[i].toUpperCase();
+            Arrays.sort(props);
+        }
+        return props;
     }
 
     static boolean errorCheck(AmazingNumber num1, Long num2, String[] props, long length) {
@@ -172,23 +261,36 @@ public class Main {
             System.out.println("The second parameter should be a natural number.");
             System.out.println();
             return false;
-        } else if (length == 4 && isNotProp(props[0]) && isNotProp(props[1])) {
-            System.out.println("The properties [" + props[0].toUpperCase() + ", " + props[1].toUpperCase() + "] are wrong.");
-            System.out.println("Available properties: [BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, EVEN, ODD, SQUARE, SUNNY]");
-            System.out.println();
-            return false;
-        } else if ((length == 3  || length == 4) && isNotProp(props[0])) {
+        }
+        if (length >= 3) {
+            return checkWrongProps(props) && checkExclusiveProps(props);
+        }
+        return true;
+    }
+
+    static boolean checkWrongProps(String[] props) {
+        ArrayList<String> errProps = new ArrayList<>();
+        for (String prop : props) {
+            if (isNotProp(prop)) {
+                errProps.add(prop);
+            }
+        }
+        if (errProps.isEmpty())
+            return true;
+        else if (errProps.size() == 1){
             System.out.println("The property [" + props[0].toUpperCase() + "] is wrong.");
-            System.out.println("Available properties: [BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, EVEN, ODD, SQUARE, SUNNY]");
-            System.out.println();
-            return false;
-        } else if ((length == 4) && isNotProp(props[1])) {
-            System.out.println("The property [" + props[1].toUpperCase() + "] is wrong.");
-            System.out.println("Available properties: [BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, EVEN, ODD, SQUARE, SUNNY]");
-            System.out.println();
-            return false;
-        } else if (length == 4 && areExclusive(props)) {
-            System.out.println("The request contains mutually exclusive properties: [" + props[0].toUpperCase() + ", " + props[1].toUpperCase() + "]");
+        } else {
+            System.out.print("The properties ");
+            System.out.print(errProps);
+            System.out.println(" are wrong.");
+        }
+        System.out.println("Available properties: " + Arrays.toString(availableProps));
+        System.out.println();
+        return false;
+    }
+
+    static boolean checkExclusiveProps(String[] props) {
+        if (hasExclusive(props)) {
             System.out.println("There are no numbers with these properties.");
             System.out.println();
             return false;
@@ -196,28 +298,48 @@ public class Main {
         return true;
     }
 
-    static boolean areExclusive(String[] props) {
-        String prop1 = props[0].toUpperCase();
-        String prop2 = props[1].toUpperCase();
-        return (prop1.equals("EVEN") && prop2.equals("ODD")) ||
-                (prop1.equals("ODD") && prop2.equals("EVEN")) ||
-                (prop1.equals("DUCK") && prop2.equals("SPY")) ||
-                (prop1.equals("SPY") && prop2.equals("DUCK")) ||
-                (prop1.equals("SUNNY") && prop2.equals("SQUARE")) ||
-                (prop1.equals("SQUARE") && prop2.equals("SUNNY"));
-    }
-
-    static void printNumsWithDoubleProps(AmazingNumber num, long count, String[] props) {
-        long fit = 0;
-        long n = num.getValue();
-        while (fit != count) {
-            AmazingNumber temp = new AmazingNumber(n);
-            if (hasProp(temp, props[0]) && hasProp(temp, props[1])) {
-                printPropsLine(temp);
-                fit++;
-            }
-            n++;
+    static boolean hasExclusive(String[] props) {
+        String str = null;
+        if(Arrays.binarySearch(props, "EVEN") >=0 &&
+                Arrays.binarySearch(props, "ODD") >=0) {
+            str = "EVEN, ODD";
+        } else if (Arrays.binarySearch(props, "DUCK") >=0 &&
+                Arrays.binarySearch(props, "SPY") >=0) {
+            str = "DUCK, SPY";
+        } else if (Arrays.binarySearch(props, "SUNNY") >=0 &&
+                Arrays.binarySearch(props, "SQUARE") >=0) {
+            str = "SUNNY, SQUARE";
+        } else if (Arrays.binarySearch(props, "HAPPY") >=0 &&
+                Arrays.binarySearch(props, "SAD") >=0) {
+            str = "HAPPY, SAD";
+        } else if(Arrays.binarySearch(props, "-EVEN") >=0 &&
+                Arrays.binarySearch(props, "-ODD") >=0) {
+            str = "-EVEN, -ODD";
+        } else if (Arrays.binarySearch(props, "-DUCK") >=0 &&
+                Arrays.binarySearch(props, "-SPY") >=0) {
+            str = "-DUCK, -SPY";
+        } else if (Arrays.binarySearch(props, "-SUNNY") >=0 &&
+                Arrays.binarySearch(props, "-SQUARE") >=0) {
+            str = "-SUNNY, -SQUARE";
+        } else if (Arrays.binarySearch(props, "-HAPPY") >=0 &&
+                Arrays.binarySearch(props, "-SAD") >=0) {
+            str = "-HAPPY, -SAD";
         }
+        for (String prop : props) {
+            for (String s : props) {
+                if (prop.equals("-" + s)) {
+                    str = prop + ", " + s;
+                    break;
+                }
+            }
+            if (str != null)
+                break;
+        }
+        if (str != null) {
+            System.out.println("The request contains mutually exclusive properties: [" + str + "]");
+            return true;
+        }
+        return false;
     }
 
     static boolean hasProp(AmazingNumber n, String prop) {
@@ -229,34 +351,33 @@ public class Main {
                 (prop.equalsIgnoreCase("gapful") && n.isGapful()) ||
                 (prop.equalsIgnoreCase("spy") && n.isSpy()) ||
                 (prop.equalsIgnoreCase("square") && n.isSquare()) ||
-                (prop.equalsIgnoreCase("sunny") && n.isSunny());
-    }
+                (prop.equalsIgnoreCase("sunny") && n.isSunny()) ||
+                (prop.equalsIgnoreCase("jumping") && n.isJumping()) ||
+                (prop.equalsIgnoreCase("happy") && n.isHappy()) ||
+                (prop.equalsIgnoreCase("sad") && n.isSad()) ||
 
-    static void printNumsWithProp(AmazingNumber num, long count, String prop) {
-        long fit = 0;
-        long n = num.getValue();
-        while (fit != count) {
-            AmazingNumber temp = new AmazingNumber(n);
-            if (hasProp(temp, prop)) {
-                printPropsLine(temp);
-                fit++;
-            }
-            n++;
-        }
+                (prop.equalsIgnoreCase("-even") && !n.isEven()) ||
+                (prop.equalsIgnoreCase("-odd") && !n.isOdd()) ||
+                (prop.equalsIgnoreCase("-buzz") && !n.isBuzz()) ||
+                (prop.equalsIgnoreCase("-duck") && !n.isDuck()) ||
+                (prop.equalsIgnoreCase("-palindromic") && !n.isPalindrome()) ||
+                (prop.equalsIgnoreCase("-gapful") && !n.isGapful()) ||
+                (prop.equalsIgnoreCase("-spy") && !n.isSpy()) ||
+                (prop.equalsIgnoreCase("-square") && !n.isSquare()) ||
+                (prop.equalsIgnoreCase("-sunny") && !n.isSunny()) ||
+                (prop.equalsIgnoreCase("-jumping") && !n.isJumping()) ||
+                (prop.equalsIgnoreCase("-happy") && !n.isHappy()) ||
+                (prop.equalsIgnoreCase("-sad") && !n.isSad());
     }
 
     static boolean isNotProp(String prop) {
         if (prop == null)
             return true;
-        return !prop.equalsIgnoreCase("even") &&
-                !prop.equalsIgnoreCase("odd") &&
-                !prop.equalsIgnoreCase("buzz") &&
-                !prop.equalsIgnoreCase("duck") &&
-                !prop.equalsIgnoreCase("palindromic") &&
-                !prop.equalsIgnoreCase("gapful") &&
-                !prop.equalsIgnoreCase("spy") &&
-                !prop.equalsIgnoreCase("square") &&
-                !prop.equalsIgnoreCase("sunny");
+        for (String s : availableProps) {
+            if (prop.equalsIgnoreCase(s))
+                return false;
+        }
+        return true;
     }
 
     static String[] getInput() {
@@ -272,7 +393,10 @@ public class Main {
                 (num.isGapful() ? ", gapful" : "") +
                 (num.isSpy() ? ", spy" : "") +
                 (num.isSquare() ? ", square" : "") +
-                (num.isSunny() ? ", sunny" : ""));
+                (num.isSunny() ? ", sunny" : "") +
+                (num.isJumping() ? ", jumping" : "") +
+                (num.isHappy() ? ", happy" : "") +
+                (num.isSad() ? ", sad" : ""));
     }
 
     static void printMultipleNumProps(AmazingNumber num, long num2) {
@@ -292,6 +416,9 @@ public class Main {
         System.out.println("\tspy: " + num.isSpy());
         System.out.println("\tsquare: " + num.isSquare());
         System.out.println("\tsunny: " + num.isSunny());
+        System.out.println("\tjumping: " + num.isJumping());
+        System.out.println("\thappy: " + num.isHappy());
+        System.out.println("\tsad: " + num.isSad());
         System.out.println();
     }
 
@@ -307,9 +434,9 @@ public class Main {
         System.out.println("- enter a natural number to know its properties;");
         System.out.println("- enter two natural numbers to obtain the properties of the list:");
         System.out.println("  * the first parameter represents a starting number;");
-        System.out.println("  * the second parameter shows how many consecutive numbers are to be printed;");
-        System.out.println("- two natural numbers and a property to search for;");
-        System.out.println("- two natural numbers and two properties to search for;");
+        System.out.println("  * the second parameter shows how many consecutive numbers are to be processed;");
+        System.out.println("- two natural numbers and properties to search for;");
+        System.out.println("- a property preceded by minus must not be present in numbers;");
         System.out.println("- separate the parameters with one space;");
         System.out.println("- enter 0 to exit.");
         System.out.println();
